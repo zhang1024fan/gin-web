9ae87bc1a539c2b67ae323b435f40ea5ee68865f
zhangfan <zhangfan@lockin.com>
2025-07-15 15:12:37 +0800

    优化初始化环境

--- src/api/index.js
+++ src/api/index.js
@@ -10,14 +10,14 @@ export default {
     // 验证码接口
     captcha() {
         return request({
-            url: "/captcha",
+            url: "/api/captcha",
             method: 'get'
         })
     },
     // 登录接口
     login(params) {
         return request({
-            url: "/login",
+            url: "/api/login",
             method: 'post',
             data: params
         })
@@ -25,7 +25,7 @@ export default {
     // post岗位
     queryPostList(params) {
         return request({
-            url: "/post/list",
+            url: "/api/post/list",
             method: 'get',
             data: params
         })
@@ -35,7 +35,7 @@ export default {
             ids
         }
         return request({
-            url: '/post/batch/delete',
+            url: '/api/post/batch/delete',
             method: 'delete',
             data: data
         })
@@ -45,20 +45,20 @@ export default {
             id
         }
         return request({
-            url: '/post/delete',
+            url: '/api/post/delete',
             method: 'delete',
             data: data
         })
     },
     querySysPostVoList() {
         return request({
-            url: '/post/vo/list',
+            url: '/api/post/vo/list',
             method: 'get'
         })
     },
     addPost(data) {
         return request({
-            url: '/post/add',
+            url: '/api/post/add',
             method: 'post',
             data: data
         })
@@ -68,14 +68,14 @@ export default {
             id
         }
         return request({
-            url: '/post/info',
+            url: '/api/post/info',
             method: 'get',
             data: data
         })
     },
     updatePost(data) {
         return request({
-            url: '/post/update',
+            url: '/api/post/update',
             method: 'put',
             data: data
         })
@@ -86,7 +86,7 @@ export default {
             postStatus
         }
         return request({
-            url: "/post/updateStatus",
+            url: "/api/post/updateStatus",
             method: 'put',
             data: data
         })
@@ -94,20 +94,20 @@ export default {
     // dept部门
     queryDeptList(params) {
         return request({
-            url: "/dept/list",
+            url: "/api/dept/list",
             method: 'get',
             data: params
         })
     },
     querySysDeptVoList() {  // 部门列表接口
         return request({
-            url: '/dept/vo/list',
+            url: '/api/dept/vo/list',
             method: 'get'
         })
     },
     addDept(data) {
         return request({
-            url: '/dept/add',
+            url: '/api/dept/add',
             method: 'post',
             data: data
         })
@@ -117,7 +117,7 @@ export default {
             id
         }
         return request({
-            url: '/dept/delete',
+            url: '/api/dept/delete',
             method: 'delete',
             data: data
         })
@@ -127,20 +127,20 @@ export default {
             id
         }
         return request({
-            url: '/dept/info',
+            url: '/api/dept/info',
             method: 'get',
             data: data
         })
     },
     deptUsers(id) {
         return request({
-            url: `/dept/users?deptId=${id}`,
+            url: `/api/dept/users?deptId=${id}`,
             method: 'get'
         })
     },
     deptUpdate(data) {
         return request({
-            url: '/dept/update',
+            url: '/api/dept/update',
             method: 'put',
             data: data
         })
@@ -148,20 +148,20 @@ export default {
     // menu菜单
     queryMenuList(params) {
         return request({
-            url: "/menu/list",
+            url: "/api/menu/list",
             method: 'get',
             data: params
         })
     },
     querySysMenuVoList() {
         return request({
-            url: "/menu/vo/list",
+            url: "/api/menu/vo/list",
             method: 'get'
         })
     },
     addMenu(data) {
         return request({
-            url: '/menu/add',
+            url: '/api/menu/add',
             method: 'post',
             data: data
         })
@@ -171,14 +171,14 @@ export default {
             id
         }
         return request({
-            url: '/menu/info',
+            url: '/api/menu/info',
             method: 'get',
             data: data
         })
     },
     menuUpdate(data) {
         return request({
-            url: '/menu/update',
+            url: '/api/menu/update',
             method: 'put',
             data: data
         })
@@ -188,7 +188,7 @@ export default {
             id
         }
         return request({
-            url: '/menu/delete',
+            url: '/api/menu/delete',
             method: 'delete',
             data: data
         })
@@ -196,20 +196,20 @@ export default {
     // role角色
     queryRoleList(params) {
         return request({
-            url: "/role/list",
+            url: "/api/role/list",
             method: 'get',
             data: params
         })
     },
     querySysRoleVoList() {
         return request({
-            url: "/role/vo/list",
+            url: "/api/role/vo/list",
             method: 'get'
         })
     },
     addRole(data) {
         return request({
-            url: '/role/add',
+            url: '/api/role/add',
             method: 'post',
             data: data
         })
@@ -219,14 +219,14 @@ export default {
             id
         }
         return request({
-            url: '/role/info',
+            url: '/api/role/info',
             method: 'get',
             data: data
         })
     },
     roleUpdate(data) {
         return request({
-            url: '/role/update',
+            url: '/api/role/update',
             method: 'put',
             data: data
         })
@@ -236,7 +236,7 @@ export default {
             id
         }
         return request({
-            url: '/role/delete',
+            url: '/api/role/delete',
             method: 'delete',
             data: data
         })
@@ -247,7 +247,7 @@ export default {
             status
         }
         return request({
-            url: "/role/updateStatus",
+            url: "/api/role/updateStatus",
             method: 'put',
             data: data
         })
@@ -257,7 +257,7 @@ export default {
             id
         }
         return request({
-            url: "/role/vo/idList",
+            url: "/api/role/vo/idList",
             method: 'get',
             data: data
         })
@@ -268,7 +268,7 @@ export default {
             menuIds
         }
         return request({
-            url: "/role/assignPermissions",
+            url: "/api/role/assignPermissions",
             method: 'put',
             data: data
         })
@@ -276,7 +276,7 @@ export default {
     // admin用户
     queryAdminList(params) {
         return request({
-            url: "/admin/list",
+            url: "/api/admin/list",
             method: 'get',
             data: params
         })
@@ -287,14 +287,14 @@ export default {
             status
         }
         return request({
-            url: "/admin/updateStatus",
+            url: "/api/admin/updateStatus",
             method: 'put',
             data: data
         })
     },
     addAdmin(data) {
         return request({
-            url: '/admin/add',
+            url: '/api/admin/add',
             method: 'post',
             data: data
         })
@@ -304,14 +304,14 @@ export default {
             id
         }
         return request({
-            url: '/admin/info',
+            url: '/api/admin/info',
             method: 'get',
             data: data
         })
     },
     adminUpdate(data) {
         return request({
-            url: '/admin/update',
+            url: '/api/admin/update',
             method: 'put',
             data: data
         })
@@ -322,7 +322,7 @@ export default {
             password
         }
         return request({
-            url: '/admin/updatePassword',
+            url: '/api/admin/updatePassword',
             method: 'put',
             data: data
         })
@@ -332,21 +332,21 @@ export default {
             id
         }
         return request({
-            url: '/admin/delete',
+            url: '/api/admin/delete',
             method: 'delete',
             data: data
         })
     },
     adminUpdatePersonal(data) {
         return request({
-            url: '/admin/updatePersonal',
+            url: '/api/admin/updatePersonal',
             method: 'put',
             data: data
         })
     },
     adminUpdatePersonalPassword(data) {
         return request({
-            url: '/admin/updatePersonalPassword',
+            url: '/api/admin/updatePersonalPassword',
             method: 'put',
             data: data
         })
@@ -354,7 +354,7 @@ export default {
     // sysLoginInfo登录日志
     querySysLoginInfoList(params) {
         return request({
-            url: "/sysLoginInfo/list",
+            url: "/api/sysLoginInfo/list",
             method: 'get',
             data: params
         })
@@ -364,14 +364,14 @@ export default {
             ids
         }
         return request({
-            url: '/sysLoginInfo/batch/delete',
+            url: '/api/sysLoginInfo/batch/delete',
             method: 'delete',
             data: data
         })
     },
     cleanSysLoginInfo() {
         return request({
-            url: '/sysLoginInfo/clean',
+            url: '/api/sysLoginInfo/clean',
             method: 'delete'
         })
     },
@@ -380,7 +380,7 @@ export default {
             id
         }
         return request({
-            url: '/sysLoginInfo/delete',
+            url: '/api/sysLoginInfo/delete',
             method: 'delete',
             data: data
         })
@@ -388,7 +388,7 @@ export default {
     // SysOperationLog操作日志
     querySysOperationLogList(params) {
         return request({
-            url: "/sysOperationLog/list",
+            url: "/api/sysOperationLog/list",
             method: 'get',
             data: params
         })
@@ -398,14 +398,14 @@ export default {
             ids
         }
         return request({
-            url: '/sysOperationLog/batch/delete',
+            url: '/api/sysOperationLog/batch/delete',
             method: 'delete',
             data: data
         })
     },
     cleanSysOperationLog() {
         return request({
-            url: '/sysOperationLog/clean',
+            url: '/api/sysOperationLog/clean',
             method: 'delete'
         })
     },
@@ -414,7 +414,7 @@ export default {
             id
         }
         return request({
-            url: '/sysOperationLog/delete',
+            url: '/api/sysOperationLog/delete',
             method: 'delete',
             data: data
         })
