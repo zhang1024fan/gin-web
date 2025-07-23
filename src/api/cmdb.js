a33e38af6ee5dad283e3372f3cc829a515bb5f7f
zhangfan <zhangfan@lockin.com>
2025-07-23 17:43:58 +0800

    新增创建主机页面，优化主机显示页面

--- src/api/cmdb.js
+++ src/api/cmdb.js
@@ -31,14 +31,58 @@ export default {
             data: data
         })
     },
-deleteCmdbGroup(id) {
-const data = {
-id
-}
-    return request({
-      url: 'cmdb/groupdelete',
-      method: 'delete',
-      data: data
-    })
-}
+    deleteCmdbGroup(id) {
+        const data = {
+            id
+        }
+        return request({
+            url: 'cmdb/groupdelete',
+            method: 'delete',
+            data: data
+        })
+    },
+
+    // 主机管理
+    createCmdbHost(data) {
+        return request({
+            url: 'cmdb/hostcreate',
+            method: 'post',
+            data: data
+        })
+    },
+    updateCmdbHost(data) {
+        return request({
+            url: 'cmdb/hostupdate',
+            method: 'put',
+            data: data
+        })
+    },
+    deleteCmdbHost(id) {
+        return request({
+            url: 'cmdb/hostdelete',
+            method: 'delete',
+            data: { id }
+        })
+    },
+    getCmdbHostList(params) {
+        return request({
+            url: 'cmdb/hostlist',
+            method: 'get',
+            params: params
+        })
+    },
+    getCmdbHostById(id) {
+        return request({
+            url: 'cmdb/hostinfo',
+            method: 'get',
+            params: { id }
+        })
+    },
+    getCmdbHostsByGroupId(groupId) {
+        return request({
+            url: 'cmdb/hostgroup',
+            method: 'get',
+            params: { groupId }
+        })
+    }
 }
